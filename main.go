322cfd7c15ded269ea1140255f01ce47203ce82a
Gleb Mojeico <42867058+mojeico@users.noreply.github.com>
2022-12-11 13:09:09 +0200

    trigger

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	_, err := fmt.Fprint(w, "Hello, Golang-app-1 ! trigger")
+	_, err := fmt.Fprint(w, "Hello, Golang-app-1 ! trigger 1")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
