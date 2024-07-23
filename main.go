0a8a18673d6afcfac7269e58aa14e85f95b0d9b5
Gleb Mojeico <gleb.m@complaer.com>
2024-07-23 18:16:28 +0300

    add blabla

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/request", requestToApp2)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -27,6 +28,35 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	_, err := fmt.Fprint(w, "Hello, Golang-app-1 ! trugger udpater 1 12")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func requestToApp2(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/request" {
+		http.NotFound(w, r)
+		return
+	}
+
+	url := os.Getenv("APP2_HOST")
+
+	if url == "" {
+		url = "http://localhost:8081"
+	}
+
+	res, err := http.Get(url + "/app2")
+
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	if err != nil {
+		w.WriteHeader(http.StatusOK)
 	}
 }
